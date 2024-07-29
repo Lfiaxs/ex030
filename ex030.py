f73ecf6f6f62313dad5dc5ee810b7428c69f898d
Lucas Fiais <lucas.fiais@hotmail.com>
2024-07-29 14:15:50 -0300

    Add files via upload

---
+++ ex030.py
@@ -0,0 +1,8 @@
+n = int(input('Digite um número qualquer: '))
+resultado = n % 2
+if resultado == 0:
+    print ('O número {} é par.'.format(n))
+else:
+    print('O número {} é ímpar.'.format(n))
+
+2
